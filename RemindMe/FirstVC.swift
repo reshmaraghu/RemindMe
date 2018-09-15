e1e226acd223ac5a9621960725e649f60722a8e2
Raghu, Reshma L <reshma.raghu@sap.com>
2018-09-15 22:57:26 +0530

    Using RAMAnimatedTabBarController with rotation animation

--- RemindMe/FirstVC.swift
+++ RemindMe/FirstVC.swift
@@ -12,19 +12,12 @@ import Spring
 class FirstVC: UIViewController {
 
 	@IBOutlet var button: SpringButton!
-	@IBOutlet var image: SpringImageView!
 
 	override func viewDidAppear(_ animated: Bool) {
-		image.animation = "morph"
-		image.curve = "easeInCirc"
-		image.duration = 2.0
-		image.animate()
-
 		button.animation = "pop"
 		button.curve = "easeIn"
 		button.duration = 2.0
 		button.repeatCount = 20
-		button.delay = 1.0
 		button.animate()
 	}
 
